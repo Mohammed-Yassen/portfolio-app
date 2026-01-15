49df580eed4f43500f1f9460dc30345f4280adc5
Mohammed-Yassen <moyaseen5100@gmail.com>
2026-01-15 23:54:19 +0300

    fix prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,16 @@
 /** @format */
-
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
 const prismaClientSingleton = () => {
-	const connectionString = `${process.env.DATABASE_URL}`;
+	// Use the Pooled URL (6543)
+	const connectionString = process.env.DATABASE_URL;
 	const pool = new Pool({ connectionString });
 	const adapter = new PrismaPg(pool);
+
+	// In modern Prisma, the adapter is passed here.
+	// We do NOT pass datasourceUrl when using an adapter.
 	return new PrismaClient({ adapter });
 };
 
